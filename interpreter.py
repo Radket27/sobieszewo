0f008c2747909d43b080adc17dadc5458724acba
Radket27 <103642613+Radket27@users.noreply.github.com>
2024-01-20 17:59:09 +0100

    Added logic

--- interpreter.py
+++ interpreter.py
@@ -23,9 +23,13 @@ def file_decode_commands(list1):
     last_var = []
     set_var = ''
     set_index_var = 0
-    for i in range(len(list1)):
+    i = 0
+    set_logical = ''
+    end_logical = False
+    while(i < len(list1)):
         has = list1[i]
-        var, last_var, set_var, set_index_var = commands_1(has,var,last_var,set_var,set_index_var)
+        var, last_var, set_var, set_index_var, list1, set_logical, end_logical = commands_1(has,var,last_var,set_var,set_index_var,list1,i,set_logical, end_logical)
+        i += 1
     return last_var
 
 def open_file(arg):
@@ -39,7 +43,7 @@ def open_file(arg):
     files.close()
     return loop
 
-def commands_1(main,var,last_var,set_var,set_index_var):
+def commands_1(main,var,last_var,set_var,set_index_var,code,location,set_logical,end_logical):
     """
     standard commands input/output
     """
@@ -49,9 +53,7 @@ def commands_1(main,var,last_var,set_var,set_index_var):
     #plugin
     from plugin import plugin, empty
     if(empty() == False):
-        var,last_var,set_var,set_index_var = plugin(command,main_text,var,last_var,set_var,set_index_var)
-    
-
+        var,last_var,set_var,set_index_var,code,location,set_logical,end_logical = plugin(command,main_text,var,last_var,set_var,set_index_var,code,location,set_logical,end_logical)
 
     if(command == 'SET'):
         try:
@@ -99,6 +101,9 @@ def commands_1(main,var,last_var,set_var,set_index_var):
     elif(command == 'PRT'):
         before = var.index(main_text)
         last_var.append(str(var[before+1]))
+    elif(command == 'NPRT'):
+        before = var.index(main_text)
+        print(str(var[before+1]))
     elif(command == 'SUB'):
         try:
             main_text = float(main_text)
@@ -198,8 +203,101 @@ def commands_1(main,var,last_var,set_var,set_index_var):
                     var[set_index_var+1] = (var[set_index_var+1] / int(var[(var.index(main_text))+1]))
                 elif(type(var[(var.index(main_text))+1]) == type(0.0)):
                     var[set_index_var+1] = (var[set_index_var+1] / float(var[(var.index(main_text))+1]))
-    
-    return var,last_var,set_var,set_index_var
+    elif(command == 'MOR'):
+        try:
+            main_text = float(main_text)
+            if(main_text < float(var[set_index_var+1])):
+                end_logical = True
+        except ValueError:
+            main_text = str(main_text)
+            if(float(var[(var.index(main_text))+1]) < float(var[set_index_var+1])):
+                end_logical = True
+        if(end_logical == False):
+            i = location
+            while((code[i] != (f'ELS {set_logical}')) or (code[i] != f'SEE {set_logical}')):
+                code.pop(i)
+                if(code[i] == f'ELS {set_logical}' or code[i] == f'SEE {set_logical}'):
+                    break
+    elif(command == 'MOQ'):
+        try:
+            main_text = float(main_text)
+            if(main_text <= float(var[set_index_var+1])):
+                end_logical = True
+        except ValueError:
+            main_text = str(main_text)
+            if(float(var[(var.index(main_text))+1]) <= float(var[set_index_var+1])):
+                end_logical = True
+        if(end_logical == False):
+            i = location
+            while((code[i] != (f'ELS {set_logical}')) or (code[i] != f'SEE {set_logical}')):
+                code.pop(i)
+                if(code[i] == f'ELS {set_logical}' or code[i] == f'SEE {set_logical}'):
+                    break
+    elif(command == 'SML'):
+        try:
+            main_text = float(main_text)
+            if(main_text > float(var[set_index_var+1])):
+                end_logical = True
+        except ValueError:
+            main_text = str(main_text)
+            if(float(var[(var.index(main_text))+1]) > float(var[set_index_var+1])):
+                end_logical = True
+        if(end_logical == False):
+            i = location
+            while((code[i] != (f'ELS {set_logical}')) or (code[i] != f'SEE {set_logical}')):
+                code.pop(i)
+                if(code[i] == f'ELS {set_logical}' or code[i] == f'SEE {set_logical}'):
+                    break
+    elif(command == 'SMQ'):
+        try:
+            main_text = float(main_text)
+            if(main_text >= float(var[set_index_var+1])):
+                end_logical = True
+        except ValueError:
+            main_text = str(main_text)
+            if(float(var[(var.index(main_text))+1]) >= float(var[set_index_var+1])):
+                end_logical = True
+        if(end_logical == False):
+            i = location
+            while((code[i] != (f'ELS {set_logical}')) or (code[i] != f'SEE {set_logical}')):
+                code.pop(i)
+                if(code[i] == f'ELS {set_logical}' or code[i] == f'SEE {set_logical}'):
+                    break
+    elif(command == 'EQL'):
+        try:
+            main_text = float(main_text)
+            if(main_text == float(var[set_index_var+1])):
+                end_logical = True
+        except ValueError:
+            main_text = str(main_text)
+            if(float(var[(var.index(main_text))+1]) == float(var[set_index_var+1])):
+                end_logical = True
+        if(end_logical == False):
+            i = location
+            while((code[i] != (f'ELS {set_logical}')) or (code[i] != f'SEE {set_logical}')):
+                code.pop(i)
+                if(code[i] == f'ELS {set_logical}' or code[i] == f'SEE {set_logical}'):
+                    break
+    elif(command == 'IFF'):
+        set_logical = main_text
+    elif(command == 'ELS'):
+        if(end_logical == True):
+            i = location
+            end1 = False
+            while(code[i] != f'SEE {set_logical}'):
+                code.pop(i)
+                if(code[i] == f'SEE {set_logical}'):
+                    break
+    elif(command == 'GOT'):
+        before = int(main_text)
+        x = location - before
+        for i in range(x+1):
+            code.insert(location+1+i,code[before+i])
+    elif(command == 'SEE'):
+        end_logical = False
+    elif(command == 'NTEX'):
+        print(str(main_text))
+    return var,last_var,set_var,set_index_var,code,set_logical,end_logical
 
 def print_something(something):
     """
